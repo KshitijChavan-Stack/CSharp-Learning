15641280319b7a6e013fd42c651d9589e98a2ba1
ikshitijhere <chavankshitij0@gmailcom>
2025-07-21 20:32:07 +0530

    Add project files.

---
+++ hello/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+// namespace is used to organize code and create a scope for identifiers
+// namespace is like a conatiner that holds classes, structs, enums, interfaces, and other namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("This is C# !");
+            Console.WriteLine("& i'm learning it.");
+            Console.ReadLine();
+        }
+
+    }
+}
+// This is a simple C# program that prints "Hello, World!" to the console.
