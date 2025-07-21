1d445783fc7504caa3a49fbd8b6a79a5f5d5fd9c
ikshitijhere <chavankshitij0@gmailcom>
2025-07-21 20:58:20 +0530

    Day1-basics of C#

--- hello/Program.cs
+++ hello/Program.cs
@@ -10,11 +10,27 @@ namespace hello
 {
     class Program
     {
+        // Main method is the entry point of the program
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variables are used to store data that can be used later in the program.
+            int jojo_age = 22; // integer variable
+
+            // Multi line comment
+            /*Data types in C#
+             integer - whole numbers
+                string - sequence of characters
+                char - single character
+                bool - true or false
+                float - single-precision floating point number
+                double - double-precision floating point number
+            */
+
+            string input = Console.ReadLine(); // reading input from the console
+            Console.WriteLine("Hello, World! this is jojo"+jojo_age);
             Console.Write("This is C# !");
             Console.WriteLine("& i'm learning it.");
+            Console.WriteLine("this is the input you entered: " + input);
             Console.ReadLine();
         }
 
